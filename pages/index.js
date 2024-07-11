8679af70b746c7dcbecd96d46ca694aa014d6fa7
veronosmani <veron.osmani@gmail.com>
2024-07-11 15:30:57 +0200

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,20 @@
-import background from '../app/background.jpg';
-import Form350 from './form350';
-import Form500 from './form500';
-import Form1000 from './form1000';
+import background from "../app/background.jpg";
+import Form350 from "./form350";
+import Form500 from "./form500";
+import Form1000 from "./form1000";
+import { Router, Routes } from "next/router";
 
 function HomePage() {
-    return <div className='relative w-full min-h-screen'>
-      <div className="min-h-screen min-w-screen flex items-center justify-center bg-no-repeat bg-cover" style={{backgroundImage: `url(${background.src})`}}>
+  return (
+    <div className="relative w-full min-h-screen">
+      <div
+        className="min-h-screen min-w-screen flex items-center justify-center bg-no-repeat bg-cover"
+        style={{ backgroundImage: `url(${background.src})` }}
+      >
         <Form350 />
       </div>
-    </div>;
-  }
-  
-  export default HomePage;
-  
+    </div>
+  );
+}
 
-  
+export default HomePage;
