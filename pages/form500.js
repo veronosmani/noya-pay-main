5702ae1de568cae3f7cd035f7fb5e2d1e3385050
veronosmani <veron.osmani@gmail.com>
2024-07-11 15:00:50 +0200

    modified:   pages/form1000.js

--- pages/form500.js
+++ pages/form500.js
@@ -34,9 +34,9 @@ export default function FormPage() {
   };
 
   return (
-    <div className="flex flex-col items-center relative py-[50px] bg-white rounded-[15px] w-[555px] h-[823px]">
+    <div className="flex flex-col items-center relative py-[50px] bg-white rounded-[15px] w-[555px] h-[750px]">
       <div className="flex flex-row justify-between items-center mx-auto space-x-4">
-        <h1 className="poppins-medium text-[30px] mt-28 mr-12">
+        <h1 className="poppins-medium text-[30px] mt-20 mr-12">
           Reservation <br />
           Details
         </h1>
@@ -57,7 +57,7 @@ export default function FormPage() {
               required
             />
           </div>
-          <div className="border-2 border-gray-600 rounded-[5px] mt-6 h-[45px] w-[420px] pl-4 flex flex-row justify-between items-center poppins-medium">
+          <div className="border-2 border-gray-600 rounded-[5px] mt-4 h-[45px] w-[420px] pl-4 flex flex-row justify-between items-center poppins-medium">
             <input
               className="w-full h-full bg-transparent outline-none uppercase placeholder:text-black"
               type="text"
@@ -67,7 +67,7 @@ export default function FormPage() {
               required
             />
           </div>
-          <div className="border-2 border-gray-600 rounded-[5px] mt-6 h-[45px] w-[420px] pl-4  fle flex-row justify-between items-center poppins-medium">
+          <div className="border-2 border-gray-600 rounded-[5px] mt-4 h-[45px] w-[420px] pl-4  fle flex-row justify-between items-center poppins-medium">
             <input
               className="w-full h-full bg-transparent outline-none uppercase placeholder:text-black"
               type="text"
@@ -77,16 +77,16 @@ export default function FormPage() {
               required
             />
           </div>
-          <div className="border-2 border-gray-600 rounded-[5px] mt-6 h-[45px] w-[420px] pl-4 uppercase flex flex-row justify-between items-center poppins-medium">
+          <div className="border-2 border-gray-600 rounded-[5px] mt-4 h-[45px] w-[420px] pl-4 uppercase flex flex-row justify-between items-center poppins-medium">
             <h1>Payment Amount</h1>
             <h1 className="bg-gray-300 w-[125px] h-[45px] rounded-[5px] border-2 border-r-0 border-gray-600 text-center font-bold flex items-center justify-center poppins-bold">
               500€
             </h1>
           </div>
         </div>
-        <div className="mb-20">
+        <div>
           <button
-            className="bg-red-800 text-[16px] font-medium text-white w-[420px] h-[45px] rounded-[5px] mt-32 poppins-medium"
+            className="bg-red-800 text-[16px] font-medium text-white w-[420px] h-[45px] rounded-[5px] mt-24 poppins-medium"
             type="submit"
           >
             Continue to payment
