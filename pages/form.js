4c2c8fc8be1d79cc367e7d3436e1a973d08836e4
veronosmani <veron.osmani@gmail.com>
2024-07-11 13:37:34 +0200

    new file:   Poppins/OFL.txt 	new file:   Poppins/Poppins-Black.ttf

--- pages/form.js
+++ pages/form.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import reservationImg from '../app/reservationImg.svg'
+import React from "react";
+import reservationImg from "../app/reservationImg.svg";
+import "../app/globals.css";
 
 export default function FormPage() {
-
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
@@ -13,53 +13,75 @@ export default function FormPage() {
         email: event.target.email.value,
         paymentAmount: event.target.paymentAmount.value,
       };
-        const response = await fetch('/api/save', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData)
-        });
-        const data = await response.json();
-        if (data.success) {
-            alert('Payment processed successfully!');
-        } else {
-            throw new Error(data.message);
-        }
+      const response = await fetch("/api/save", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      if (data.success) {
+        alert("Payment processed successfully!");
+      } else {
+        throw new Error(data.message);
+      }
     } catch (error) {
-        console.error("Failed to process payment:", error);
-        alert('Failed to process payment. Please try again.');
+      console.error("Failed to process payment:", error);
+      alert("Failed to process payment. Please try again.");
     }
-};
+  };
 
   return (
-    <div className='flex relative p-20 bg-white rounded-[15px] w-[555px] h-[823px]'>
-      <div>
-      <h1 className=''>Reservation Details</h1>
-      <div className="w-[150px] h-[265px]" style={{backgroundImage: `url(${reservationImg.src})`}} ></div>
+    <div className="flex flex-col items-center relative py-[50px] bg-white rounded-[15px] w-[555px] h-[823px]">
+      <div className="flex flex-row justify-between items-center mx-auto space-x-4">
+        <h1 className="font-medium text-[30px] mt-28 mr-12">
+          Reservation <br />
+          Details
+        </h1>
+        <div
+          className="w-[160px] h-[265px] bg-no-repeat"
+          style={{ backgroundImage: `url(${reservationImg.src})` }}
+        ></div>
       </div>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" required />
+          <div className="border-2 border-gray-600 rounded-[5px] mt-10 h-[45px] w-[420px] pl-4 pt-2 uppercase">
+            <label htmlFor="name">Name / Surname</label>
+            <input
+              className="justify-center"
+              type="text"
+              id="name"
+              name="name"
+              required
+            />
+          </div>
+          <div className="border-2 border-gray-600 rounded-[5px] mt-6 h-[45px] w-[420px] pl-4 pt-2 uppercase">
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" name="email" required />
+          </div>
+          <div className="border-2 border-gray-600 rounded-[5px] mt-6 h-[45px] w-[420px] pl-4 pt-2 uppercase">
+            <label htmlFor="phoneNumber">Phone Number</label>
+            <input type="tel" id="phoneNumber" name="phoneNumber" required />
+          </div>
+          <div className="border-2 border-gray-600 rounded-[5px] mt-6 h-[45px] w-[420px] pl-4 uppercase flex flex-row justify-between items-center">
+            <label htmlFor="paymentAmount">Payment Amount</label>
+            <input
+             className="bg-gray-300 w-[125px] h-[45px] rounded-[5px] border-2 border-black text-center"
+              type="number"
+              id="paymentAmount"
+              name="paymentAmount"
+              required
+            />
+          </div>
         </div>
-        <div>
-          <label htmlFor="surname">Surname:</label>
-          <input type="text" id="surname" name="surname" required />
-        </div>
-        <div>
-          <label htmlFor="phoneNumber">Phone Number:</label>
-          <input type="tel" id="phoneNumber" name="phoneNumber" required />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
-        </div>
-        <div>
-          <input type="hidden" id="paymentAmount" name="paymentAmount" value="100" />
-        </div>
-        <div>
-          <button type="submit">Continue</button>
+        <div className="mb-20">
+          <button
+            className="bg-red-800 text-[16px] font-medium text-white w-[420px] h-[45px] rounded-[5px] mt-32"
+            type="submit"
+          >
+            Continue to payment
+          </button>
         </div>
       </form>
     </div>
